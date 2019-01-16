b931b55f2752c68cf82710fffcde953c8475e0ec
allen_edv <allendeedv@gmail.com>
2019-01-16 13:28:33 +0100

    Update animal.php

--- php/animal.php
+++ php/animal.php
@@ -9,17 +9,17 @@
         public $raza;
         public $edad;
         public $sexo;
-        public $imagen;
-        public $img_perfil;
+        // public $imagen;
+        // public $img_perfil;
 
-        public static function crearAnimal($nombre,$especie, $raza, $edad, $sexo,$imagen,$img_perfil){
+        public static function crearAnimal($nombre,$especie, $raza, $edad, $sexo){
 
              //Metemos en la variable conexión la llamada a la class Conexion de conexion.php y usamos el método getConexion()
              $conexion=Conexion::getConexion();
 
              //insertamos los datos del animal en la BBDD
              $q=<<<sql
-                INSERT INTO animal (nombre,especie, raza, edad, sexo, imagen, img_perfil) VALUES ('$nombre','$especie', '$raza', '$edad', '$sexo', '$imagen', '$img_perfil')
+                INSERT INTO animal (nombre,especie, raza, edad, sexo) VALUES ('$nombre','$especie', '$raza', '$edad', '$sexo')
 sql;
             $conexion->query($q) or die("ERRORRRR");
 
@@ -47,8 +47,8 @@ sql;
 				$animal->raza = $rows['raza'];
 				$animal->edad = $rows['edad'];
                 $animal->sexo = $rows['sexo'];
-                $animal->img_p = $rows['imagen'];
-                $animal->img = $rows['img_perfil'];
+                // $animal->img_p = $rows['imagen'];
+                // $animal->img = $rows['img_perfil'];
 
                 $animales[] = $animal;
         }
