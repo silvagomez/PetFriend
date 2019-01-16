f2b214e0a5fdce41ee1bf8be9adce542e205164f
silvagomez <38257521+silvagomez@users.noreply.github.com>
2019-01-16 12:51:48 +0100

    links update

--- interface/animales.php
+++ interface/animales.php
@@ -25,7 +25,7 @@
 
 	<!--Zona para los scripts-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-	<script src="../js/buscador.js" defer></script>
+	<script src="../js/buscador.js" async defer></script>
 </head>
 
 <body>
